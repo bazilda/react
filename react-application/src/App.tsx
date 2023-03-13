b88e6df28c95f3cee067311a3135ef2f229fe59d
bazilda <bazilda@gmail.com>
2023-03-13 20:03:33 +0200

    HW-32 last

--- react-application/src/App.tsx
+++ react-application/src/App.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
-//import logo from './logo.svg';
-
 import './App.css';
 import {Timer} from './components/Timer';
 function App() {
-const column:React.CSSProperties={display: "flex",flexDirection: "column"}
-const row:React.CSSProperties={display: "flex",flexDirection: "row"}
-  return <div>
-    <div style={column} >
-    <Timer  cityCountry='Israel'/>
+  const column: React.CSSProperties = {display: "flex", flexDirection: "column",
+  height: "90vh", justifyContent: "center", background: "blue"};
+  const row: React.CSSProperties = {display: "flex", flexDirection: "row",
+   justifyContent: "space-around", background:"blue",color:"white"};
+  return <div style={column}>
+    <div style={row}>
+       <Timer cityCountry='Israel'/>
     <Timer cityCountry='Sydney'/>
-  </div>
-  <div style={row} >
+    </div>
+   <div style={row}>
     <Timer cityCountry='Mexico'/>
     <Timer cityCountry='Chicago'/>
-    </div>
-    </div>
+   </div>
+   </div>
 }
 
 export default App;
