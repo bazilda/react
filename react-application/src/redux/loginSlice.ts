94ca56bc300aa76871a922f4bca9ef1fcb62c4b1
bazilda <bazilda@gmail.com>
2023-03-20 22:16:32 +0200

    HW-34

---
+++ react-application/src/redux/loginSlice.ts
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState: { userLogin: string } = {
+    userLogin: '';
+}
+const loginSlice = createSlice({
+    initialState,
+    name: "login",
+    reducers: {
+        login: (state, data) => {
+            state.userLogin = data.payload;
+        },
+        logout: (state) => {
+            state.userLogin = initialState.userLogin;
+        }
+    }
+});
--- react-application/src/redux/store.ts
+++ react-application/src/redux/store.ts
@@ -3,6 +3,6 @@ import { counterReducer } from "./counterSlice";
 export const store: any = configureStore({
     reducer: {
        count: counterReducer,
-
+        login:
     }
 })
